c7bec699d6300fd5aa1fddf1e56318e8435065fc
GregFJr <finney101690@gmail.com>
2025-07-28 10:06:16 -0700

    finished home banner, updated the file structure

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-
-
+import React from "react";
+import bannerImg from "../assets/home/desktop/image-hero-phone.png";
 
 function HomePage() {
-    return(
-        <div className='homeWrapper' >
-            <section className='hero'>
-                fart
-            </section>
+  return (
+    <div className="homeWrapper">
+      <section className="hero">
+        <div className="banner-msg">
+          <p>Award winning custom designs and digital branding solutions</p>
+          <p>
+            With over 10 years in the industry, we are experienced in creating
+            fully responsive websites, app design, and engaging brand
+            experiences. Find out more about our services.
+          </p>
         </div>
-    )
+        <div className="banner-img">
+        </div>
+      </section>
+
+      <section className="home-grid">
+        <div className="wed-design"></div>
+        <div className="app-design"></div>
+        <div className="graphic-design"></div>
+      </section>
+    </div>
+  );
 }
 
 export default HomePage;
